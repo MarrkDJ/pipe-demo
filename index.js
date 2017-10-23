06d2b61adf65ea1b5f999ded3ff993c7fbbd8331
david kuiken <dhkkuik@gmail.com>
2017-10-23 12:40:50 +0200

    Test fix for node crash

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const env = {
   ENV_COLOR: env_color
 };
 
-app.set('port', (env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 app.get('/api/getEnvironment', function(request, response){
   const envVar = {
@@ -32,5 +35,5 @@ app.get('/*', function(req, res) {
 
 app.listen(app.get('port'), function(){
 	console.log('Node app is running on port', app.get('port'));
-  console.log('NODE_ENV: '+ env.NODE_ENV);
+  console.log('NODE_ENV: '+ process.env.NODE_ENV);
 });
